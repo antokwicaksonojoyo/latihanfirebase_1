5d4d4d9fa1aafb3af9e8c967f212dfca76f5b82c
Antok Nurwicaksono <72114629+antokwicaksonojoyo@users.noreply.github.com>
2021-07-01 19:09:28 +0700

    - Menambah fungsi update dan delete
    - Menambah Fungsi Update
    - Menambah fungsi delete

--- app/src/main/java/com/antok/latihanfirebase_1/RecyclerViewAdapter.kt
+++ app/src/main/java/com/antok/latihanfirebase_1/RecyclerViewAdapter.kt
@@ -1,7 +1,11 @@
 package com.antok.latihanfirebase_1
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,13 +17,13 @@ import data_mahasiswa
 //Class Adapter ini Digunakan Untuk Mengatur Bagaimana Data akan Ditampilkan
 class RecyclerViewAdapter( private val listMahasiswa: ArrayList<data_mahasiswa>, context: Context) :
     RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
-    private val context: Context
+    private var context: Context
 
     //ViewHolder Digunakan Untuk Menyimpan Referensi Dari View-View
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val NIM: TextView
         val Nama: TextView
-         val Jurusan: TextView
+        val Jurusan: TextView
         val ListItem: LinearLayout
 
         init {//Menginisialisasi View yang terpasang pada layout RecyclerView kita
@@ -50,6 +54,33 @@ class RecyclerViewAdapter( private val listMahasiswa: ArrayList<data_mahasiswa>,
         holder.ListItem.setOnLongClickListener(object : View.OnLongClickListener {
             override fun onLongClick(v: View?): Boolean {
 //Kodingan untuk fungsi Edit dan Delete, yang dibahas pada Tutorial Berikutnya.
+                holder.ListItem.setOnLongClickListener { view ->
+                    val action = arrayOf("Update", "Delete")
+                    val alert: AlertDialog.Builder = AlertDialog.Builder(view.context)
+                    alert.setItems(action, DialogInterface.OnClickListener { dialog, i ->
+                        when (i) {
+                            0 -> {
+                                /* Berpindah Activity pada halaman layout updateData dan mengambil data pada listMahasiswa, berdasarkan posisinya untuk dikirim pada activity selanjutnya */
+                                val bundle = Bundle()
+                                bundle.putString("dataNIM", listMahasiswa[position].nim)
+                                bundle.putString("dataNama", listMahasiswa[position].nama)
+                                bundle.putString("dataJurusan", listMahasiswa[position].jurusan)
+                                bundle.putString("getPrimaryKey", listMahasiswa[position].key)
+                                val intent = Intent(view.context, UpdateData::class.java)
+                                intent.putExtras(bundle)
+                                context.startActivity(intent)
+                            }
+                            1 -> {
+                                //Menggunakan interface untuk mengirim data mahasiswa, yang akan dihapus
+                                listener?.onDeleteData(listMahasiswa.get(position), position)
+                            }
+                        }
+                    })
+                    alert.create()
+                    alert.show()
+                    true
+                }
+
                 return true
             }
         })
@@ -64,4 +95,19 @@ class RecyclerViewAdapter( private val listMahasiswa: ArrayList<data_mahasiswa>,
     init {
         this.context = context
     }
+    //Membuat Interfece
+    interface dataListener {
+        fun onDeleteData(data: data_mahasiswa?, position: Int)
+    }
+
+    //Deklarasi objek dari Interfece
+    var listener: dataListener? = null
+
+    //Membuat Konstruktor, untuk menerima input dari Database
+    init {
+        this.context = context
+        this.listener = context as MyListData  //menambahkan baris ini saja
+    }
+
+
 }
